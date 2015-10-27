924cc148b22447ffd7b7d617d1d1a2fbcd28d2a3
HugoIP <hugo@amsmedia.com>
2015-10-27 14:56:20 -0600

    Version inicial con UI

---
+++ MiBiblioteca/Assets/Scripts/Planet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planet : MonoBehaviour {
+	public GameObject center;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Permite girar a un objeto al rededor de otro
+		//lo busque en google como: rotate around unity
+		transform.RotateAround(center.transform.position , Vector3.up, speed * Time.deltaTime);
+	}
+}
